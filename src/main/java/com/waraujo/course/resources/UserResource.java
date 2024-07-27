c01f904abada34cb31869502bbc9742f68faf2d4
Willian Araujo <wil.araujo20@gmail.com>
2024-07-27 19:59:10 +0100

    User delete

--- src/main/java/com/waraujo/course/resources/UserResource.java
+++ src/main/java/com/waraujo/course/resources/UserResource.java
@@ -1,11 +1,11 @@
 package com.waraujo.course.resources;
 
 import java.net.URI;
-import java.net.URL;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +44,12 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
