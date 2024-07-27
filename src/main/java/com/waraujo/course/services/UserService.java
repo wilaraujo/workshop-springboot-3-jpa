2eb749d651278d5fef4b1e6073365d529e43e578
Willian Araujo <wil.araujo20@gmail.com>
2024-07-27 22:12:39 +0100

    Exception handling - delete

--- src/main/java/com/waraujo/course/services/UserService.java
+++ src/main/java/com/waraujo/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.waraujo.course.entities.User;
 import com.waraujo.course.repositories.UserRepository;
+import com.waraujo.course.services.exceptions.DatabaseException;
 import com.waraujo.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +32,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		if (repository.existsById(id)) {
+		    try {
+				repository.deleteById(id);
+			} catch (DataIntegrityViolationException e) {
+				throw new DatabaseException(e.getMessage());
+			}
+		} else {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public User update(Long id, User obj) {
