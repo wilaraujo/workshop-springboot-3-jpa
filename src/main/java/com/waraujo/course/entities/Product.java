aae3c86fa55db79ab2c306e7354757bc63ed2529
Willian Araujo <wil.araujo20@gmail.com>
2024-07-27 16:01:20 +0100

    Product-OrderItem one-to-many association

--- src/main/java/com/waraujo/course/entities/Product.java
+++ src/main/java/com/waraujo/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,6 +29,9 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	@ManyToMany
 	@JoinTable(name = "tb_product_category",
@@ -97,6 +103,15 @@ public class Product implements Serializable{
 		return categories;
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
--- src/main/java/com/waraujo/course/repositories/OrderItemRepository.java
+++ src/main/java/com/waraujo/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.waraujo.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.waraujo.course.entities.OrderItem;
+import com.waraujo.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
